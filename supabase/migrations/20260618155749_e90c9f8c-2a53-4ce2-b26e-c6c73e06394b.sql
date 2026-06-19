e23424afa76bb7087aee1874f767c5bfac1d4ca8
Muhammadamin <145181015+Muhammadamin-001@users.noreply.github.com>
2026-06-19 12:05:04 +0500

    fail
    sss

--- supabase/migrations/20260618155749_e90c9f8c-2a53-4ce2-b26e-c6c73e06394b.sql
+++
@@ -1,9 +0,0 @@
-
-ALTER FUNCTION public.set_updated_at() SET search_path = public;
-
-REVOKE EXECUTE ON FUNCTION public.has_role(uuid, public.app_role) FROM PUBLIC, anon, authenticated;
-GRANT EXECUTE ON FUNCTION public.has_role(uuid, public.app_role) TO authenticated, service_role;
-
-REVOKE EXECUTE ON FUNCTION public.handle_new_user() FROM PUBLIC, anon, authenticated;
-
-CREATE POLICY "deny all telegram_messages" ON public.telegram_messages FOR ALL TO authenticated USING (false) WITH CHECK (false);
